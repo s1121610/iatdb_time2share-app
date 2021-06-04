6a07b38fa9fedc498c4e9a89da43beb392ee95ae
s1121610 <max@LAPTOP-F2F4E214.localdomain>
2021-06-04 22:28:01 +0200

    bugfix block message

--- resources/views/User/blocked/blocked--message.blade.php
+++ resources/views/User/blocked/blocked--message.blade.php
@@ -1,9 +1,11 @@
 @extends('Lenen.default')
 @section('content')
 @include('Lenen.navbar')
-<h2>Uw account is geblokkeerd</h2>
-<p>Neem contact op met de beheerder voor meer informatie</p>
-<form method="POST" action="/logout">
-    @csrf
-    <button class="u-button u-button--primary">Uitloggen</button>
-</form>
+<section class="block-message">
+    <h2>Uw account is geblokkeerd</h2>
+    <p>Neem contact op met de beheerder voor meer info.</p>
+    <form method="POST" action="/logout">
+        @csrf
+        <button class="u-button u-button--primary">Uitloggen</button>
+    </form>
+</section>
